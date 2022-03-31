6254f2ca962de7a7c8de9587a84d82a8e1ff5e6a
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-01 01:33:46 +0430

    Update website.controller.js

--- Src/Server/Controllers/website.controller.js
+++ Src/Server/Controllers/website.controller.js
@@ -2,12 +2,13 @@ const axios = require("axios");
 const { User } = require("discord-info")
 const { Database } = require('beta.db')
 const db = new Database('./Src/Public/data.json')
+require('dotenv').config({ path: './Src/Server/config.env' });
 
 
 exports.index = async (req, res) => {
     res.render("../Pages/index.ejs", {
-        title:"SoSiS - Imagine a bot",
-        icon:"https://cdn.discordapp.com/avatars/762378493929455617/f3cac0f6d38cd184ac25005df8711753.png?size=2048"
+        title: "SoSiS - Imagine a bot",
+        icon: "https://cdn.discordapp.com/avatars/762378493929455617/f3cac0f6d38cd184ac25005df8711753.png?size=2048"
     });
 };
 
@@ -19,12 +20,12 @@ exports.love = async (req, res) => {
         const user = '806903080754872372'
         const ids = Object.keys(res.data)
 
-        User({ UserID: user, BotToken: 'ODE5ODgzMDc4OTM0NTkzNTQ2.YEtFng.jQWSdZj0H-PiRc4QyDV_NouVHNE' }).then((user) => {
+        User({ UserID: user, BotToken: process.env.BotToken }).then((user) => {
             ids.forEach(id => {
                 if (id.startsWith(user.id)) {
                     const target = id.split(".").slice("")[1];
 
-                    User({ UserID: target, BotToken: 'ODE5ODgzMDc4OTM0NTkzNTQ2.YEtFng.jQWSdZj0H-PiRc4QyDV_NouVHNE' }).then((response) => {
+                    User({ UserID: target, BotToken: process.env.botToken }).then((response) => {
                         result.push({
                             user: {
                                 name: user.tag,
@@ -44,6 +45,8 @@ exports.love = async (req, res) => {
 
     setTimeout(() => {
         res.render("../Pages/love.ejs", {
+            title: 'SoSiS - Love DataBase',
+            icon: "https://cdn.discordapp.com/avatars/762378493929455617/f3cac0f6d38cd184ac25005df8711753.png?size=2048",
             result,
         });
     }, 6000);
