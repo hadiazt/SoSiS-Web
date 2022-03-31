a954b8d6fb6da02683f076ba0ba188fd862bfc33
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-31 18:50:51 +0430

    Update server.js

--- src/Server/server.js
+++ src/Server/server.js
@@ -8,6 +8,11 @@ app.set("views", "src/Pages");
 
 app.use(express.static("../Public"));
 app.use("/", require("./Routes/index.js"))
+app.use("/main", require("./Routes/index.js"))
+app.use("/security", require("./Routes/index.js"))
+app.use("/downloader", require("./Routes/index.js"))
+app.use("/contributors", require("./Routes/index.js"))
+
 
 
 const PORT = 3000;
