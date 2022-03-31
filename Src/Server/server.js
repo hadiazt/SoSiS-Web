935b6592011df666ad903931c318544b08aba34f
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-31 19:15:00 +0430

    Update server.js

--- Src/Server/server.js
+++ Src/Server/server.js
@@ -1,40 +1,11 @@
-const express = require("express"),
-    expressIp = require('express-ip'),
-    compression = require("compression"),
-    websiteRouter = require('./Routes/website.routing'),
+const app = require('./app');
 
-    app = express();
-
-
-app.set("view engine", "ejs");
-app.set("views", "Src/Pages");
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static("../Public"));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
-app.use(express.json({ limit: '10kb' }));
-app.use(compression());
-app.use(expressIp().getIpInfoMiddleware);
-
-app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App running on  http://localhost:${port}`);
 });
 
-app.use('/', websiteRouter);
-app.use("/main", websiteRouter);
-app.use("/security", websiteRouter);
-app.use("/downloader", websiteRouter);
-app.use("/contributors", websiteRouter);
-
-app.use("/main/love", websiteRouter);
-
-app.use(require("./controllers/error.controller").get404);
 
 process.on('unhandledRejection', err => {
-    console.log(err);
+    console.log(err)
 });
-
-module.exports = app;
-
