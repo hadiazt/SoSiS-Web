f089abca06fac08ca8a568681f6c4429337e00fd
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-01 01:32:34 +0430

    Update server.js

--- Src/Server/server.js
+++ Src/Server/server.js
@@ -2,7 +2,6 @@ const app = require('./app');
 
 require('dotenv').config({ path: './Src/Server/config.env' });
 
-
 app.listen(process.env.port, () => {
     console.log(`App running on  http://localhost:${port}`);
 });
