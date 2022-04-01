8b1b4b884c7a564e55724476d629494f21142183
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-01 11:09:53 +0430

    Update error.controller.js

--- Src/Server/Controllers/error.controller.js
+++ Src/Server/Controllers/error.controller.js
@@ -1,4 +1,32 @@
-exports.get404 = async (req, res) =>
-{
-    return res.render("../Pages/error.ejs");
+exports.get404 = async (req, res) => {
+    return res.render("../Pages/error.ejs", {
+        title: "SoSiS - Page Not Found",
+        icon: "https://cdn.discordapp.com/avatars/762378493929455617/f3cac0f6d38cd184ac25005df8711753.png?size=2048",
+        pages: [
+            {
+                name: '/',
+                address: ''
+            },
+            {
+                name: '/love',
+                address: ''
+            },
+            {
+                name: '/main',
+                address: ''
+            },
+            {
+                name: '/security',
+                address: ''
+            },
+            {
+                name: '/downloader',
+                address: ''
+            },
+            {
+                name: '/contributors',
+                address: ''
+            }
+        ]
+    });
 };
