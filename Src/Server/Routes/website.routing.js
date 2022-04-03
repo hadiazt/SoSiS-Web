9df0f05d3e7789d5bb6da64462f3b39486d569d0
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-03 15:17:35 +0430

    Update website.routing.js

--- Src/Server/Routes/website.routing.js
+++ Src/Server/Routes/website.routing.js
@@ -6,6 +6,7 @@ const express = require('express'),
 
 router.get('/', Controller.index);
 router.get('/love', Controller.love);
+router.get('/sosis', Controller.sosis);
 
 
 module.exports = router;
