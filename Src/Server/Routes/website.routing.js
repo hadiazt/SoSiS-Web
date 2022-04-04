1f0aaf2d03a357aac701a1b90afb8e0e73d74a72
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-04 21:24:21 +0430

    Update website.routing.js

--- Src/Server/Routes/website.routing.js
+++ Src/Server/Routes/website.routing.js
@@ -10,6 +10,7 @@ router.get('/love', Controller.love);
 router.get('/sosis', Controller.sosis);
 router.get('/security', Controller.security);
 router.get('/downloader', Controller.downloader);
+router.get("/contributors", Controller.contributors);
 
 
 module.exports = router;
